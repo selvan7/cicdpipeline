d9ac76c90002b2011177e70e7828306554a54b90
selvan7 <777tamil777@gmail.com>
2022-12-21 16:40:38 +0530

    AppCenter Implementation

--- app/src/main/java/com/example/cicdpipeline/MainActivity.kt
+++ app/src/main/java/com/example/cicdpipeline/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.cicdpipeline
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCenter.start(
+            application, "c23c2986-53bf-46aa-bc99-7fafc74cfc5b",
+            Analytics::class.java, Crashes::class.java
+        )
+
     }
 }
